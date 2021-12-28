bb3b5b2c4331132db89aa91d49f803482c9535e3
mehmet surucu <mehmetsmarket@gmail.com>
2021-12-28 20:21:23 +0300

    28Dec

---
+++ Test2.java
@@ -0,0 +1,21 @@
+package JavaHomeWork;
+
+
+	public class Test2 {
+		
+		public static void main(String[] args) {
+			
+			Test1 t =new Test1(11.5, 7, 9);
+			
+			t.powerSwitch();
+			// System.out.println(t.decreaseVolume()); // 11
+			
+			// System.out.println(t.channelTuning(2)); // 54.89
+			
+			System.out.println(t.decreaseVolume()); // 11
+		}
+		
+	}
+
+
+
